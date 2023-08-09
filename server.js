a7f6e63793f3b09c185dbde7026254162b503e90
dominoto <zubac76@gmail.com>
2023-08-09 20:18:20 +0200

    Try to fix login

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 const app = express();
-const knex = require('knex');
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false // not secure for production, just for local development without users
-        }
-    }
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // not secure for production, just for local development without users
+    },
+  },
 });
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working') })
-app.post('/signin', signin.handleSignin(db, bcrypt)) //one way of passing arguments
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })//another way of passing arguments
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.get("/", (req, res) => {
+  res.send("it is working");
+});
+// app.post('/signin', signin.handleSignin(db, bcrypt)) //one way of passing arguments
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+}); //another way of passing arguments
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
-})
+  console.log(`app is running on port ${process.env.PORT}`);
+});
